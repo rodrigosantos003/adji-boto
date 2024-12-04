6247d7488b28e42674d8de007628cdb4ba5d7f3e
Rodrigo Santos <rodrigo_santos003@outlook.com>
2024-12-04 14:26:51 +0000

    Refactored performance measurement functions

--- desempenho.lisp
+++ desempenho.lisp
@@ -2,72 +2,33 @@
 ;;; Funções para calcular as medidas de desempenho dos algoritmos
 ;;; Autores: João Fernandes e Rodrigo Santos
 
-(defun penetrancia (no-final abertos fechados tamanho-espaco-busca)
-  "Calcula a penetrância dado o nó final, listas de abertos e fechados, e o tamanho do espaço de busca."
-  (let* ((n-nos-visitados (+ (length abertos) (length fechados)))  ; Total de nós visitados
-         (penetrancia (/ (* 100.0 n-nos-visitados) tamanho-espaco-busca)))  ; Penetrância
-    (list :no-final no-final               ; Retorna o nó final
-          :n-nos-visitados n-nos-visitados ; Nós visitados
-          :tamanho-espaco-busca tamanho-espaco-busca ; Espaço de busca
-          :penetrancia (round penetrancia)))) ; Penetrância arredondada
+;; Penetrância
+(defun penetrancia (comprimento-caminho total-nos)
+  "Calcula a penetrância de um algortimo (P = L/T)"
+  (/ comprimento-caminho total-nos)
+)
 
+;; Fator de ramificação
+(defun fator-ramificacao (comprimento-caminho total-nos a b tolerancia)
+  "Calcula o fator de ramificação de um algoritmo (B + B^2 + … + B^L = T)"
+  (metodo-bisseccao (lambda (b) (funcao-f b comprimento-caminho total-nos)) a b tolerancia)
+)
 
-(defun tamanho-espaco-busca (tabuleiro)
-  "Calcula o tamanho do espaço de busca do tab"
-  (fatorial (length (flatten tabuleiro))))
+(defun soma-potencias (ramificacao comprimento-caminho)
+  (if (= comprimento-caminho  1)
+      ramificacao
+      (+ (expt ramificacao comprimento-caminho) (soma-potencias ramificacao (- comprimento-caminho 1)))))
 
-(defun fatorial (n)
-  "Calcula o fatorial de n."
-  (if (<= n 1)
-      1
-      (* n (fatorial (- n 1)))))
+(defun funcao-f (ramificacao comprimento-caminho total-nos)
+  (- (soma-potencias ramificacao comprimento-caminho) total-nos))
 
-(defun fator-ramificacao (L total-nos &key (accuracy 0.0001))
-  "Calcula o fator de ramificação médio B usando Newton-Raphson."
-  (let* ((f (lambda (B)
-              "Função f(B): soma dos termos - total-nos."
-              (- (loop for i from 1 to L
-                       sum (expt B i))
-                 total-nos)))
-         (f-prime (lambda (B)
-                    "Derivada f'(B): soma das derivadas dos termos."
-                    (loop for i from 1 to L
-                          sum (* i (expt B (1- i)))))))
-    (multiple-value-bind (B iteracoes)
-        (Newton-Raphson f f-prime 0.1 10.0 :accuracy accuracy)
-      (format t "Fator de ramificação médio calculado: ~a (em ~d iterações)~%"
-              B iteracoes)
-      B)))
-
-
-(defun Newton-Raphson
-       (f
-        f-prime
-        x-left
-        x-right
-        &key
-        (accuracy (* 10.0 single-float-epsilon))
-        (maximum-number-of-iterations 20)
-        (prevent-bracket-jumping-p t))
- (assert (< x-left x-right))
-  (let ((x (* 0.5 (+ x-left x-right)))
-        delta-x denom-for-accuracy-test)
-    (dotimes (j maximum-number-of-iterations
-                (if (not (cerror "returns solution so far"
-                                 "exceeding maximum number of iterations"))
-                  (values x maximum-number-of-iterations)))
-      (setf delta-x (/ (funcall f x)  (funcall f-prime x)))
-      (setf denom-for-accuracy-test (+ (abs x)
-                                       (abs (decf x delta-x))))
-      (cond
-       (prevent-bracket-jumping-p
-        (if (< x x-left) (setf x x-left))
-        (if (> x x-right) (setf x x-right))
-        (if (< (/ (abs delta-x) denom-for-accuracy-test) accuracy)
-          (return (values x (1+ j)))))
-       ((<= x-left x x-right)
-        (if (< (/ (abs delta-x) denom-for-accuracy-test) accuracy)
-          (return (values x (1+ j)))))
-       (t
-        (error "jumped out of brackets")
-        )))))
+(defun metodo-bisseccao (fun a b tolerancia)
+  (if (< (- b a) tolerancia)(/ (+ a b) 2)
+      (let* ((c (/ (+ a b) 2))
+             (fa (funcall fun a))
+             (fc (funcall fun c)))
+        (cond
+            ((= fc 0) c)
+            ((< (* fa fc) 0) (metodo-bisseccao fun c b tolerancia))
+            (t (metodo-bisseccao fun a c tolerancia)))))
+)
